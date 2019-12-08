895b5725678aba4e485d5ad6fe5a004a681ff889
Name-does-not-exist <58401061+Name-does-not-exist@users.noreply.github.com>
2019-12-08 19:03:18 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	int Peter = 0;
+    SAVE_VARIBLE_NAME(Peter);
+    cout << VARIBLE_NAME << endl;
+	
+	
+	int x;
+	int y;
+	int 25;
+	25=25;
+	cin>>x;
+	y=x*x*x*x*x+x*x*x*x+x*x*x+x*x+25
+	cout<<y;
+	
+	{
+		int x;
+		cin>>x;
+		cout <x;
+	}
+	
+	{
+		int x;
+		cin>>x;
+		int Go ;
+		cin>>Go ;
+	    cout<< Go *x;
+		
+	}
+	
+	
+	{
+		int x;
+		int y;
+		int z;
+		cin>>x>>y>>z;
+		x<y<z
+		if (){
+		cout<<"z";
+		cout<<"x":
+			
+	}
+	else {
+		cout<<"x";
+		cout<<"z";
+	}		
+	}
+	{
+		int x;
+		int y;
+		int z;
+		int o;
+		cin>>x>>y>>z>>o;
+		o<z<y<x;
+		if(){
+		cout<<x;
+		cout<<o;
+		}
+	else{
+		cout<<o;
+		cout<<x;
+	}	
+		
+	}
+	
+	return 0;
+}
